91b0032a2ee550f6f72e02078b623383ef6a877b
Howard Roark <howardroark@users.noreply.github.com>
2015-01-16 18:51:38 -0500

    Use proper environment path.

--- stackstrap.sh
+++ stackstrap.sh
@@ -4,7 +4,7 @@ cd /tmp
 
 curl -L https://raw.githubusercontent.com/stackstrap/salt-bootstrap/fork/bootstrap-salt.sh | sudo sh -s -- git develop
 
-wget https://raw.githubusercontent.com/stackstrap/boot/master/files/minion
+wget https://raw.githubusercontent.com/stackstrap/environment/master/files/minion
 cp minion /etc/salt/minion
 
 sudo salt-call state.highstate -l debug
