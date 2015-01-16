d00fac90ef4726bc2445a77305afefaf4984ac14
Howard Roark <howie@codingcoop.org>
2015-01-16 16:24:08 -0500

    install salt as well

--- strap.sh
+++ strap.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 cd /tmp
+
+wget -O install_salt.sh https://raw.githubusercontent.com/stackstrap/salt-bootstrap/fork/bootstrap-salt.sh
+sudo sh install_salt.sh
+
 wget https://raw.githubusercontent.com/stackstrap/boot/master/files/minion
 cp minion /etc/salt/minion
+
 sudo salt-call state.highstate -l debug
