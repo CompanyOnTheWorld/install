fd3ff5ee830b7890a4763b5e84392b2f5489c88f
Howard Roark <howardroark@users.noreply.github.com>
2015-01-16 23:28:34 -0500

    Update stackstrap.sh

--- stackstrap.sh
+++ stackstrap.sh
@@ -5,7 +5,7 @@ cd /tmp
 wget https://raw.githubusercontent.com/stackstrap/install/master/files/minion
 mv minion /etc/salt/minion
 
-sudo apt-get install  build-essential python-dev python-git python-virtualenv
+sudo apt-get install build-essential python-dev python-git python-virtualenv
 sudo pip install --upgrade pip 
 pip install hjson
 
