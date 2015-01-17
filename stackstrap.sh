0b11779a1f5a915d4a424efc4430cc6d4392d258
Howard Roark <howardroark@users.noreply.github.com>
2015-01-16 23:47:53 -0500

    Update stackstrap.sh

--- stackstrap.sh
+++ stackstrap.sh
@@ -5,8 +5,6 @@ cd /tmp
 wget https://raw.githubusercontent.com/stackstrap/install/master/files/minion
 mv minion /etc/salt/minion
 
-apt-get install -y build-essential 
-apt-get install -y python-dev 
 apt-get install -y python-git 
 apt-get install -y python-virtualenv
 
