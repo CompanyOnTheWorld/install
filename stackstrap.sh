188aff6a45fc01800ead446d2f6d96e1883dc088
Howard Roark <howie@codingcoop.org>
2015-01-21 07:18:13 -0500

    get script moving files into place

--- stackstrap.sh
+++ stackstrap.sh
@@ -3,8 +3,10 @@
 cd /tmp
 
 wget https://raw.githubusercontent.com/stackstrap/install/master/files/minion
+wget https://raw.githubusercontent.com/stackstrap/install/master/files/srv/environment/top.sls
 mv minion /etc/salt/minion
-mv files/srv /srv
+mkdir -p /srv/environment
+mv top.sls /srv/environment/.
 
 apt-get install -y python-git
 apt-get install -y python-virtualenv
