583fe5c963e0ab0e7f1a5cc0286641d78e2b6f24
Howard Roark <howie@codingcoop.org>
2015-03-22 11:46:03 -0400

    simplify

--- stackstrap.sh
+++ stackstrap.sh
@@ -2,16 +2,8 @@
 
 cd /tmp
 
-wget https://raw.githubusercontent.com/stackstrap/install/master/files/minion
-wget https://raw.githubusercontent.com/stackstrap/install/master/files/srv/top.sls
-mv minion /etc/salt/minion
-mkdir -p /srv
-mv top.sls /srv/.
-
 apt-get install -y python-git
 apt-get install -y python-virtualenv
 apt-get install -y python-setuptools
 
 pip install hjson
-
-sudo salt-call state.highstate -l debug
