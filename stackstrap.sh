06b81f74823d52aa271f6d1787381444a2293072
Howard Roark <howardroark@users.noreply.github.com>
2015-04-16 15:24:21 -0400

    Update stackstrap.sh

--- stackstrap.sh
+++ stackstrap.sh
@@ -2,8 +2,13 @@
 
 cd /tmp
 
+apt-get update
+apt-get install -y build-essentials
+apt-get install -y python
+apt-get install -y python-dev
 apt-get install -y python-git
-apt-get install -y python-virtualenv
+apt-get install -y python-pip
 apt-get install -y python-setuptools
+apt-get install -y python-virtualenv
 
 pip install hjson
