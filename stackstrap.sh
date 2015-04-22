115dbed84e8631a74073a32a99cc094b05e42568
Howard Roark <howie@codingcoop.org>
2015-04-22 17:54:06 -0400

    simplify

--- stackstrap.sh
+++ stackstrap.sh
@@ -20,13 +20,7 @@ cd /tmp
 
 apt-get update
 
-apt-get install -y build-essential
-apt-get install -y python
-apt-get install -y python-dev
 apt-get install -y python-git
-apt-get install -y python-pip
-apt-get install -y python-setuptools
-apt-get install -y python-virtualenv
 apt-get install -y unzip
 
 pip install hjson
