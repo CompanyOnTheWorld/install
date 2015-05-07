600069224391e65fa86de188b0aef09cdb8d1058
Howard Roark <howie@codingcoop.org>
2015-05-07 16:06:40 -0400

    trying something

--- stackstrap.sh
+++ stackstrap.sh
@@ -23,7 +23,7 @@ if [ $PROJECT_CONFIG ] ; then
     echo "Project:"
     echo "${PROJECT_CONFIG}" | python -m json.tool
 
-    salt-call state.highstate --retcode-passthrough --out json --log-level=info pillar="${PROJECT_CONFIG}"
+    salt-call state.highstate --retcode-passthrough --out json --log-level=quiet --log-file=/var/log/salt/minion pillar="${PROJECT_CONFIG}"
 else
-    salt-call state.highstate --retcode-passthrough --out json --log-level=info
+    salt-call state.highstate --retcode-passthrough --out json --log-level=quiet --log-file=/var/log/salt/minion
 fi
